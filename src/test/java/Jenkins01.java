4a5245323f32578e47e0019a941b68c1618d7bb3
sercosgun <ercosgunserdar@gmail.com>
2022-11-15 15:10:00 +0300

    15.11.2022

--- src/test/java/Jenkins01.java
+++ src/test/java/Jenkins01.java
@@ -3,6 +3,6 @@ public class Jenkins01 {
     public static void main(String[] args) {
           
         System.out.println("Merhaba Jenkins");
-
+        System.out.println("Merhaba 2 Jenkins");
     }
 }
